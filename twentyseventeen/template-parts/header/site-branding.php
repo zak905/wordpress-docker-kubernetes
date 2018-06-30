10d5d4c0c7a982b19a9447ad9e32d5b75d14012f
zak905 <zakaria.amine88@gmail.com>
2018-06-30 11:38:15 +0200

    change site title

--- twentyseventeen/template-parts/header/site-branding.php
+++ twentyseventeen/template-parts/header/site-branding.php
@@ -16,7 +16,7 @@
 
 		<div class="site-branding-text">
 			<?php if ( is_front_page() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Webhooks demo</a></h1>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php endif; ?>
